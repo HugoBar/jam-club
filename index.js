3ead2e9e10588ab547110a1842a889f1a96c7db4
HugoBar <33893943+HugoBar@users.noreply.github.com>
2024-12-13 11:56:40 +0000

    add db connection

--- index.js
+++ index.js
@@ -1,6 +1,24 @@
 const express = require('express');
 const router = express.Router();
 const app = express();
+require('dotenv').config()
+
+const mongoose = require('mongoose');
+
+// Replace <username>, <password>, and <dbname> with your actual credentials
+const dbURL = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}/?retryWrites=true&w=majority&appName=${process.env.DB_NAME}`
+
+mongoose
+  .connect(dbURL)
+  .then((result) => {
+    console.log('Connected to MongoDB');
+    app.listen(process.env.DB_PORT, () => {
+      console.log('Server started on port ***');
+    });
+  })
+  .catch((err) => {
+    console.error('Could not connect to MongoDB:', err);
+  });
 
 // Middleware and routes will be added here
 const songRouter = require('./routes/song.router');
