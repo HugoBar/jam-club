5be3025094ef4dae1f7d5d4df2e1a23c6f0b507b
HugoBar <33893943+HugoBar@users.noreply.github.com>
2024-12-17 11:33:46 +0000

    Add timestamps

--- models/song.model.js
+++ models/song.model.js
@@ -1,8 +1,13 @@
-const mongoose = require("mongoose")
+const mongoose = require("mongoose");
 
-const dailySongSchema = new mongoose.Schema({
-  title: { type: String, required: true },
-  artist: { type: String, unique: true },
-})
+const dailySongSchema = new mongoose.Schema(
+  {
+    title: { type: String, required: true },
+    artist: { type: String, unique: true },
+  },
+  {
+    timestamps: true,
+  }
+);
 
-module.exports = mongoose.model("dailySong", dailySongSchema)
+module.exports = mongoose.model("dailySong", dailySongSchema);
--- models/user.model.js
+++ models/user.model.js
@@ -1,9 +1,14 @@
 const mongoose = require("mongoose");
 
-const userSchema = new mongoose.Schema({
-  username: { type: String, unique: true, required: true },
-  password: { type: String, unique: true },
-  nickname: { type: String, unique: true, required: true },
-});
+const userSchema = new mongoose.Schema(
+  {
+    username: { type: String, unique: true, required: true },
+    password: { type: String, unique: true },
+    nickname: { type: String, unique: true, required: true },
+  },
+  {
+    timestamps: true,
+  }
+);
 
 module.exports = mongoose.model("User", userSchema);
