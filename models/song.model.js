538308b1828c6752fe4ca8b13fc70a6565a5055e
HugoBar <33893943+HugoBar@users.noreply.github.com>
2024-12-19 15:55:23 +0000

    Fix schema definitions

--- models/song.model.js
+++ models/song.model.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const dailySongSchema = new mongoose.Schema(
   {
     title: { type: String, required: true },
-    artist: { type: String, unique: true },
+    artist: { type: String, required: true },
   },
   {
     timestamps: true,
