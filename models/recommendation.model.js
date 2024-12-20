34411136931d950b9987609b5889a40948e2f892
HugoBar <33893943+HugoBar@users.noreply.github.com>
2024-12-20 13:41:06 +0000

    Add user reference to recommendations

--- models/recommendation.model.js
+++ models/recommendation.model.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const recommendationSchema = new mongoose.Schema(
   {
-    userId: { type: Number, required: true },
+    userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
     title: { type: String, required: true },
     artist: { type: String, required: true},
   },
@@ -10,5 +10,19 @@ const recommendationSchema = new mongoose.Schema(
     timestamps: true,
   }
 );
+
+recommendationSchema.statics.findTodayByUsers = function(userIds) {
+  // Get today's start and end date
+  const startOfDay = new Date();
+  startOfDay.setHours(0, 0, 0, 0); // Start of today
+
+  const endOfDay = new Date();
+  endOfDay.setHours(23, 59, 59, 999); // End of today
+  console.log(this)
+  return this.find({
+    userId: { $in: userIds },
+    createdAt: { $gte: startOfDay, $lte: endOfDay }
+  });
+};
 
 module.exports = mongoose.model("Recommendation", recommendationSchema);
