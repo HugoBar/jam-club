3c85ba743ede6d2d9c52c0011b223f5ea1649341
HugoBar <33893943+HugoBar@users.noreply.github.com>
2025-01-13 14:15:51 +0000

    Refactor track model

--- models/track.model.js
+++ models/track.model.js
@@ -2,12 +2,7 @@ const mongoose = require("mongoose");
 
 const trackSchema = new mongoose.Schema(
   {
-    name: { type: String, required: true },
-    artists: [{ type: String, required: true }],
-    external_urls: { type: mongoose.Schema.Types.Mixed, default: {} },
-    // cover: { type: String, required: true },
-    // releaseYear: { type: String, required: true },
-    // genres: [{ type: String, required: true }]
+    spotify_id: { type: String, required: true }
   },
   {
     timestamps: true,
