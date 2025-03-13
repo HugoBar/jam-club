319a30c81ac1ffe458fb104d255febeb8dcd18c0
Hugo Barreiro <hugomdbarreiro@gmail.com>
2025-03-13 14:06:52 +0000

    Add allowed origins

--- config/cors/cors.js
+++ config/cors/cors.js
@@ -1,8 +1,16 @@
 const cors = require('cors');
-
 require('dotenv').config();
 
+const allowedOrigins = process.env.ALLOWED_ORIGINS.split(',');
+
 const corsOptions = {
+  origin: (origin, callback) => {
+    if (allowedOrigins.includes(origin) || !origin) {
+      callback(null, true); 
+    } else {
+      callback(new Error('Not allowed by CORS'));  
+    }
+  },
   credentials: true,
   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
   allowedHeaders: 'Content-Type,Authorization'
