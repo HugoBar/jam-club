f800d0378470047f9e7d3502ac95e8c80e1921d3
Hugo Barreiro <hugomdbarreiro@gmail.com>
2025-03-21 13:25:21 +0000

    Remove logs

--- middlewares/auth.middleware.js
+++ middlewares/auth.middleware.js
@@ -5,13 +5,11 @@ async function verifyToken(req, res, next) {
   // Retrieve the token from the Authorization header
   const authHeader = req.headers.authorization;
   let token;
-  console.log("refreshToken first", req.cookies.refreshToken)
 
   if (authHeader) {
-    token = authHeader.split(' ')[1];
+    token = authHeader.split(" ")[1];
   }
 
-  console.log(token)
   // If no token is provided, respond with a 401 Unauthorized status
   if (!token) {
     return res.status(401).json({ error: "Access token not provided" });
@@ -23,30 +21,33 @@ async function verifyToken(req, res, next) {
     req.userId = decoded.userId;
     return next();
   } catch (error) {
-  console.log("refreshToken second", req.cookies.refreshToken)
-
     // If access token verification fails, check the refresh token
     const refreshToken = req.cookies.refreshToken;
     if (!refreshToken) {
       return res.status(401).json({ error: "Refresh token not provided" });
     }
 
-    console.log("refreshToken", refreshToken)
     try {
       // Verify the refresh token
-      const decoded = jwt.verify(refreshToken, process.env.JWT_REFRESH_SECRET_KEY);
-      const newAccessToken = jwt.sign({ userId: decoded.userId, iat: Math.floor(Date.now() / 1000) }, process.env.JWT_SECRET_KEY, {
-        expiresIn: "10s", 
-      });
+      const decoded = jwt.verify(
+        refreshToken,
+        process.env.JWT_REFRESH_SECRET_KEY
+      );
+      const newAccessToken = jwt.sign(
+        { userId: decoded.userId, iat: Math.floor(Date.now() / 1000) },
+        process.env.JWT_SECRET_KEY,
+        {
+          expiresIn: "10s",
+        }
+      );
 
       // Set the new access token in the response header
-      res.setHeader('new-access-token', `Bearer ${newAccessToken}`);
+      res.setHeader("new-access-token", `Bearer ${newAccessToken}`);
 
       // Attach the userId to the request object
       req.userId = decoded.userId;
       return next();
     } catch (error) {
-      console.log("error", error)
       return res.status(403).json({ error: "Invalid refresh token" });
     }
   }
